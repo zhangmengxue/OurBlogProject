1b1066b8984674a46e0fb9f41ec1c7f4a68e1b05
zhangmengxue <673013891@qq.com>
2014-10-17 00:44:33 +0800

    newest project

---
+++ js/index.js
@@ -0,0 +1,15 @@
+
+$(window).load(function(){
+   $('.yun').animate({right:"180px"},18000);
+});
+$(window).load(function(){
+	$('.fish').animate({right:"180px"},18000);
+});
+$(window).load(function(){
+      $('.pan_pic').toggleClass('panpan_pic');
+    });
+
+
+
+
+
